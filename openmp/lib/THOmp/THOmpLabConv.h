4afd883af5922041e2609ea37cfe2e9759bc72e5
Pierre-Francois Laquerre <pierre.francois@gmail.com>
2011-10-13 15:58:17 -0400

    Add missing dllexport for Windows.

--- openmp/lib/THOmp/THOmpLabConv.h
+++ openmp/lib/THOmp/THOmpLabConv.h
@@ -1,5 +1,15 @@
 #include "TH.h"
 
+#ifdef _WIN32
+# ifdef THOmp_EXPORTS
+#  define THOMP_API __declspec(dllexport)
+# else
+#  define THOMP_API __declspec(dllimport)
+# endif
+#else
+# define THOMP_API /**/
+#endif
+
 #define THOmpLab_(NAME)   TH_CONCAT_4(THOmp,Real,Lab_,NAME)
 
 #include "generic/THOmpLabConv.h"
